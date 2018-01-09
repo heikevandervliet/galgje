7d17fb50ac092ed1f7693b04bc6e28d77fce7edd
Heike van der Vliet <heikevandervliet@hotmail.com>
2018-01-09 13:42:18 +0100

    update3

--- galgje/Galgje.java
+++ galgje/Galgje.java
@@ -5,7 +5,14 @@ public class Galgje {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("test");
+		System.out.println("Welkom bij galgje");
+		System.out.println("Het te raden woord:");
+		
+		String teRadenWoord = "Qien";
+		
+		System.out.println(teRadenWoord);
+		
+		
 
 	}
 
