dcad56fe5862f8b1c1e2d640c1ae231275717c4c
Heike van der Vliet <heikevandervliet@hotmail.com>
2018-01-09 13:58:01 +0100

    volgende

--- galgje/Galgje.java
+++ galgje/Galgje.java
@@ -1,14 +1,29 @@
 package galgje;
 
+import java.util.Scanner;
+
 public class Galgje {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		System.out.println("Welkom bij galgje");
 		System.out.println("Het te raden woord:");
 		
 		String teRadenWoord = "Qien";
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+		
+		if(teRadenWoord.contains(str)) {
+			System.out.println("ja");
+		} else {
+			System.out.println("nee");
+		}
+		
+		
 		
 		System.out.println(teRadenWoord);
 		
